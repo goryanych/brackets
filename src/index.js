0480d483bcf075daa8e1dbcc3bc15ee1fc3d9ec3
goryanych <kolohryvyi@gmail.com>
2021-02-28 20:21:28 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs;
+
+  for (let i = 1; i; i--) {
+    for (const elem of bracketsConfig) {
+      pairs = elem.join('');
+
+      if (str.includes(pairs)) {
+        str = str.replace(pairs, '');
+        i++;
+      }
+    }
+  }
+
+  return !str.length;
 }
